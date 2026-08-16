d7e187a1da2b61b07c8daf06fe496a059cc28d9d
cafesincuchara <vicente.herrera.565656@gmail.com>
2026-08-16 19:14:13 -0400

    fix(kafka): trust event DTO package and wire listener container factory explicitly

--- src/main/java/com/spotify/group/infrastructure/kafka/KafkaConfig.java
+++ src/main/java/com/spotify/group/infrastructure/kafka/KafkaConfig.java
@@ -6,6 +6,7 @@ import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
@@ -34,8 +35,17 @@ public class KafkaConfig {
 
     @Bean
     public ConsumerFactory<String, Object> consumerFactory(KafkaProperties properties, ObjectMapper objectMapper) {
+        JsonDeserializer<Object> valueDeserializer = new JsonDeserializer<>(objectMapper);
+        valueDeserializer.addTrustedPackages(TRUSTED_PACKAGE);
         Map<String, Object> consumerProps = properties.buildConsumerProperties();
-        consumerProps.put(JsonDeserializer.TRUSTED_PACKAGES, TRUSTED_PACKAGE);
-        return new DefaultKafkaConsumerFactory<>(consumerProps, new StringDeserializer(), new JsonDeserializer<>(objectMapper));
+        return new DefaultKafkaConsumerFactory<>(consumerProps, new StringDeserializer(), valueDeserializer);
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, Object> kafkaListenerContainerFactory(
+            ConsumerFactory<String, Object> consumerFactory) {
+        ConcurrentKafkaListenerContainerFactory<String, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory);
+        return factory;
     }
 }
